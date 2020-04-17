84edc92649fd3cd2aae00ee6e8bb3f41cab80298
Gabriel Ferrari <gabrieljunior88@hotmail.com>
2020-04-17 00:14:50 -0300

    Front-End finished

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -14,17 +14,27 @@ export default function Register() {
   const [city, setCity] = useState('');
   const [uf, setUf] = useState('');
 
+  const history = useHistory();
 
-  function handleRegister(e) {
+  async function handleRegister(e) {
     e.preventDefault();
 
-    const data = ({
+    const data = {
       name,
       email,
       whatsapp,
       city,
-      uf,}
-    )
+      uf,
+    };
+
+    try{
+      const response = await api.post('ongs', data);
+
+      alert(`Seu ID de acesso: ${response.data.id}`);
+      history.push('/');
+    } catch (err) {
+      alert('Error no cadastro tente novamente');
+    }
   }
 
 
